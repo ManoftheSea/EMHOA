b26eb2674a96931f2df7c9a3ac9dbc5e196ea04f
Derek LaHousse <derek@seaofdirac.org>
2024-04-19 13:34:34 -0400

    add tools to reproduce PDFs

---
+++ flake.nix
@@ -0,0 +1,71 @@
+{
+  description = "Project environment for latex";
+  nixConfig.bash-prompt-suffix = "(latex) ";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    flake-utils.url = github:numtide/flake-utils;
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  } @ inputs: let
+    systems = [
+      "x86_64-linux"
+    ];
+  in
+    flake-utils.lib.eachSystem systems (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      tex = pkgs.texlive.combine {
+        inherit
+          (pkgs.texlive)
+          #hyperref
+          #infwarerr
+          #kvoptions
+          latex-bin
+          latexmk
+          #pdftexcmds
+          scheme-basic
+          titlesec
+          ;
+      };
+    in {
+      devShells.default = pkgs.mkShellNoCC {
+        name = "LaTeX environment";
+        packages = [
+          tex
+        ];
+        shellHook = ''
+          echo "Welcome to $name"
+        '';
+      };
+
+      formatter = pkgs.alejandra;
+      packages = {
+        default = self.packages.${system}.documents;
+        documents = pkgs.stdenvNoCC.mkDerivation rec {
+          pname = "EMHOA-Docs";
+          version = "0.1";
+          src = ./src;
+          buildInputs = [pkgs.coreutils tex];
+          phases = ["unpackPhase" "buildPhase" "installPhase"];
+          # suppressoptionalinfo removes random PDF id from lualatex
+          buildPhase = ''
+            export PATH="${pkgs.lib.makeBinPath buildInputs}";
+            mkdir -p .cache/texmf-var
+            env TEXMFHOME=.cache TEXMFVAR=.cache/texmf-var \
+              SOURCE_DATE_EPOCH=${toString self.lastModified} \
+              latexmk -interaction=nonstopmode -pdf -lualatex \
+              -pretex="\pdfvariable suppressoptionalinfo 512\relax" \
+              -usepretex \
+              Bylaws.tex Covenants.tex
+          '';
+          installPhase = ''
+            mkdir -p $out
+            cp *.pdf $out/
+          '';
+        };
+      };
+    });
+}
